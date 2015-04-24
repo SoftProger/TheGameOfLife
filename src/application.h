374bb40fa0007cf33ecbad47f151e61df2fec35e
SoftProger <andrewpo4ta@gmail.com>
2015-04-25 00:39:37 +0300

    Initial commit

--- src/application.h
+++ src/application.h
@@ -1,3 +1,17 @@
+/*
+    Copyright (C) 2015 Andrey Puzankov
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef APPLICATION_H
 #define APPLICATION_H
 #include <string>
@@ -9,7 +23,7 @@ class Application
     const std::string lifeQuotes[18] = {"Life isn't about finding yourself. Life is about creating yourself.",
                                       "Life is really simple, but we insist on making it complicated.",
                                      "Life is 10% what happens to us and 90% how we react to it.",
-                                     "When I hear somebody sigh, ‘Life is hard,’ I am always tempted to ask, ‘Compared to what?’",
+                                     "When I hear somebody sigh, 'Life is hard' I am always tempted to ask, 'Compared to what?'",
                                       "You only live once, but if you do it right, once is enough.",
                                       "Everything you can imagine is real.",
                                       "Life is like riding a bicycle. To keep your balance, you must keep moving.",
